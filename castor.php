50a9c4afc18963b4d32a8060e0100ed827012e48
Simon Chabrier <simonchabrier@gmail.com>
2025-01-17 19:59:34 +0100

    update command add more options

--- castor.php
+++ castor.php
@@ -1,18 +1,23 @@
 <?php
 
 use Castor\Attribute\AsTask;
+
+use function Castor\check;
 use function Castor\io;
 
 #[AsTask(description: 'Cloner un dépôt, configure et lance un projet Symfony avec serveurs et navigation')]
 function cloneProject(): void
 {
+
+    $installNpm = false;
+
     io()->title('Initialisation du projet Symfony');
 
     $currentDir = getcwd();
     io()->text("Dossier actuel : $currentDir");
 
     // demander si on veut remonter d'un niveau pour cloner le dépôt hors du dossier courant
-    $upLevel = io()->confirm('Voulez-vous cloner le dépôt dans un dossier parent ?', true);
+    $upLevel = io()->confirm('Voulez-vous cloner le dépôt dans un dossier parent ?', false);
     if ($upLevel) {
         chdir('..');
         $currentDir = getcwd();
@@ -25,8 +30,19 @@ function cloneProject(): void
     $projectPath = $currentDir . DIRECTORY_SEPARATOR . $projectName;
 
     if (is_dir($projectPath)) {
-        io()->error("Le dossier '$projectName' existe déjà. Supprimez-le ou choisissez un autre nom.");
-        return;
+        $delete = io()->confirm("Le dossier '$projectName' existe déjà. voulez vous le supprimer ?", false);
+        if ($delete) {
+            exec("rm -rf $projectName", $output, $returnVar);
+            if ($returnVar === 0) {
+                io()->success("Dossier '$projectName' supprimé.");
+            } else {
+                io()->error("Échec de la suppression du dossier '$projectName'.");
+                return;
+            }
+        } else {
+            io()->error("Le dossier '$projectName' existe déjà. Veuillez le supprimer ou choisir un autre nom. La commande est annulée.");
+            return;
+        }
     }
 
     io()->section('On clone le dépôt Git...');
@@ -108,6 +124,15 @@ function cloneProject(): void
         return;
     }
 
+    // informer l'utilisateur de la compatibilité des versions 
+    io()->section('Vérification de la compatibilité des versions PHP...');
+    $phpVersion = checkSystemPhp();
+    $phpProjectMaxVersion = checkProjectPhpMaxVersion();
+    // si la verison php du système est supérieure à la version php requise par le projet on retourne un warning pour informer l'utilisateur que des dépandances peuvent ne pas être compatibles
+    if (version_compare($phpVersion, $phpProjectMaxVersion, '>')) {
+        io()->warning("La version PHP du système est supérieure à la version PHP requise par le projet. Certaines dépendances peuvent ne pas être compatibles et le script peut échouer.");
+    }
+
     // Étape 3 : Installation des dépendances avec Composer
     $installComposer = io()->confirm('Voulez-vous installer les dépendances Composer maintenant ?', true);
 
@@ -117,11 +142,21 @@ function cloneProject(): void
         if ($returnVar === 0) {
             io()->success('Dépendances Composer installées.');
         } else {
-            io()->error('Échec de l\'installation des dépendances Composer.');
-            return;
+            io()->warning('Échec de l\'installation des dépendances Composer car certaines dépendances sont pas compatibles avec la version PHP du système.');
+            // demander si on veut recommander en faisant un composer update
+            $updateComposer = io()->confirm('Voulez-vous essayer de mettre à jour les dépendances Composer avec "composer update" ?', false);
+            if ($updateComposer) {
+                exec('composer update --with-all-dependencies', $output, $returnVar);
+                if ($returnVar === 0) {
+                    io()->success('Dépendances Composer mises à jour.');
+                } else {
+                    io()->error('Échec de la mise à jour des dépendances Composer.');
+                    return;
+                }
+            } else {
+                return;
+            }
         }
-    } else {
-        io()->success('Installation des dépendances Composer ignorée.');
     }
 
     // Étape 4 : Installation des dépendances NPM
@@ -154,13 +189,44 @@ function cloneProject(): void
     }
 
     // Étape 6 : Mise à jour du schéma de la base de données
-    io()->section('Mise à jour du schéma Doctrine...');
-    exec('php bin/console doctrine:schema:update --force --complete', $output, $returnVar);
-    if ($returnVar === 0) {
-        io()->success('Schéma Doctrine mis à jour.');
+    io()->section('Mise à jour de la base de données...');
+
+    // regarder si il y a un dossier migrations
+    if (is_dir('migrations')) {
+        // demander si on veut
+        $migrate = io()->confirm('Voulez-vous exécuter les migrations Doctrine ?', true);
+        if ($migrate) {
+            exec('php bin/console doctrine:migrations:migrate --no-interaction', $output, $returnVar);
+            if ($returnVar === 0) {
+                io()->success('Migrations Doctrine exécutées.');
+            } else {
+                io()->error('Échec de l\'exécution des migrations Doctrine.');
+                return;
+            }
+        }
     } else {
-        io()->error('Échec de la mise à jour du schéma Doctrine.');
-        return;
+        exec('php bin/console doctrine:schema:update --force --complete', $output, $returnVar);
+        if ($returnVar === 0) {
+            io()->success('Schéma Doctrine mis à jour.');
+        } else {
+            io()->error('Échec de la mise à jour du schéma Doctrine.');
+            return;
+        }
+    }
+
+    // est ce qu'il y a des fixtures
+    if (is_dir('src/DataFixtures')) {
+        // demander si on veut
+        $loadFixtures = io()->confirm('Voulez-vous charger les fixtures Doctrine ?', true);
+        if ($loadFixtures) {
+            exec('php bin/console doctrine:fixtures:load --no-interaction', $output, $returnVar);
+            if ($returnVar === 0) {
+                io()->success('Fixtures Doctrine chargées.');
+            } else {
+                io()->error('Échec du chargement des fixtures Doctrine.');
+                return;
+            }
+        }
     }
 
     // Étape 7 : Vérification des droits sur le dossier public
@@ -183,10 +249,15 @@ function cloneProject(): void
 
     // demander si on veur lancer les serveurs et ouvrir le navigateur
     $launchServers = io()->confirm('Voulez-vous lancer les serveurs Symfony et ouvrir le navigateur maintenant ?', true);
-    if ($launchServers) {
+    if ($launchServers && $installNpm) {
+        runProject(); // appel de runProject pour lancer les serveurs
+        runAssets(); // appel de runAssets pour lancer les serveurs
+        sleep(5); // Attente de 5 secondes
+        openProject();
+    } elseif ($launchServers && !$installNpm) {
         runProject(); // appel de runProject pour lancer les serveurs
         sleep(5); // Attente de 5 secondes
-        openProject(); // appel de openProject pour ouvrir le navigateur
+        openProject();
     } else {
         io()->text('Vous pouvez lancer le serveur de développement avec la commande "php castor.php runProject"');
         io()->text('Vous pouvez ouvrir le navigateur avec la commande "php castor.php openProject"');
@@ -201,13 +272,93 @@ function cloneProject(): void
     io()->success('Initialisation du projet terminée.');
 }
 
+// Commande pour identifier si Xamp Mamp ou Wamp est installé
+#[AsTask(description: 'Vérifier si XAMPP, MAMP ou WAMP est installé')]
+function checkLocalServer(): void
+{
+    io()->title('Vérification de XAMPP, MAMP ou WAMP');
+    $system = checkOS();
+
+    // si mac on cherche MAMP
+    if ($system === 'macOS') {
+        $isMamp = is_dir('/Applications/MAMP');
+        if ($isMamp) {
+            io()->success('MAMP est installé.');
+        } else {
+            io()->warning('MAMP n\'est pas installé.');
+        }
+    } elseif ($system === 'Windows') {
+        // si windows on cherche XAMPP ou WAMP
+        $isXampp = is_dir('C:\xampp');
+        $isWamp = is_dir('C:\wamp');
+        if ($isXampp) {
+            io()->success('XAMPP est installé.');
+        } elseif ($isWamp) {
+            io()->success('WAMP est installé.');
+        } else {
+            io()->warning('Ni XAMPP ni WAMP n\'est installé.');
+        }
+    } elseif ($system === 'Linux') {
+        // si linux on cherche LAMP
+        $isLamp = is_dir('/opt/lampp');
+        if ($isLamp) {
+            io()->success('LAMP est installé.');
+        } else {
+            io()->warning('LAMP n\'est pas installé.');
+        }
+    }
+}
+
+// chercher la version php activée sur le système pour voir si elles correspondent
+#[AsTask(description: 'Vérifier la version PHP installée sur le système')]
+function checkSystemPhp(): string
+{
+    io()->title('Vérification de la version PHP');
+    return phpversion();
+}
+
+// Vérifier la version de php demandé dans le composer.json
+#[AsTask(description: 'Vérifier la version PHP requise dans le fichier composer.json')]
+function checkProjectPhpMaxVersion(): string
+{
+    io()->title('Vérification de la version PHP requise par le coposer.json');
+    // regarder toutes les versions de php dans le composer.json requise pour chaque package on cherche par exemple ça : "php": ">=7.2.5", et retourne la version la plus haute
+    preg_match_all('/"php": ">=([0-9.]+)"/', file_get_contents('composer.json'), $matches);
+    $phpVersions = $matches[1] ?? [];
+    $phpMaxVersion = max($phpVersions);
+    io()->success("Version PHP requise par le projet a été trouvée das composer.json : $phpMaxVersion");
+    return $phpMaxVersion;
+}
+
+// command epour vérifier le système d'exploitation
+#[AsTask(description: 'Vérifier le système d\'exploitation')]
+function checkOS(): string
+{
+    io()->title('Vérification du système d\'exploitation');
+
+    $isMac = strtoupper(substr(PHP_OS, 0, 3)) === 'DAR';  // macOS
+    $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';  // Windows
+    $isLinux = strtolower(PHP_OS) === 'linux';  // Linux
+
+    if ($isMac) {
+        io()->success('Système d\'exploitation : macOS');
+    } elseif ($isWindows) {
+        io()->success('Système d\'exploitation : Windows');
+    } elseif ($isLinux) {
+        io()->success('Système d\'exploitation : Linux');
+    } else {
+        io()->warning('Système d\'exploitation non reconnu.');
+    }
+
+    // on retourne le nom du système d'exploitation
+    return $isMac ? 'macOS' : ($isWindows ? 'Windows' : ($isLinux ? 'Linux' : 'Inconnu'));
+}
+
 #[AsTask(description: 'Lancer les serveurs Symfony et de surveillance des assets')]
 function runProject(): void
 {
     // Vérifier le système d'exploitation
-    $isMac = strtoupper(substr(PHP_OS, 0, 3)) === 'DAR';  // macOS
-    $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';  // Windows
-    $isLinux = strtolower(PHP_OS) === 'linux';  // Linux
+    $ystem = checkOS();
 
     // Étape 1 : Lancer le serveur Symfony uniquement s'il n'est pas déjà en cours
     io()->section('Démarrage du serveur Symfony...');
@@ -215,7 +366,8 @@ function runProject(): void
     $serverRunning = false;
     $pid = null;
 
-    if ($isMac) {
+    // ouverture du terminal pour lancer le serveur
+    if ($ystem === 'macOS') {
         // Vérifier si le serveur Symfony est déjà en cours
         exec('lsof -i :8000', $output, $returnVar);
         if (!empty($output)) {
@@ -224,7 +376,7 @@ function runProject(): void
             preg_match('/\d+/', $output[0], $matches);
             $pid = $matches[0] ?? null;
         }
-    } elseif ($isWindows) {
+    } elseif ($ystem === 'Windows') {
         // Vérifier si le serveur Symfony est déjà en cours (port 8000 utilisé)
         exec('netstat -ano | findstr :8000', $output, $returnVar);
         if (!empty($output)) {
@@ -233,7 +385,7 @@ function runProject(): void
             preg_match('/\d+/', end($output), $matches);
             $pid = $matches[0] ?? null;
         }
-    } elseif ($isLinux) {
+    } elseif ($ystem === 'Linux') {
         // Vérifier si le serveur Symfony est déjà en cours
         exec('lsof -i :8000', $output, $returnVar);
         if (!empty($output)) {
@@ -244,14 +396,15 @@ function runProject(): void
         }
     }
 
+    // Si un serveur est déjà en cours, le tuer et le redémarrer
     if ($serverRunning && $pid !== null) {
         // Si le serveur est déjà en cours, le tuer
         io()->warning('Le serveur Symfony est déjà en cours d\'exécution, arrêt du processus...');
-        if ($isMac) {
+        if ($ystem === 'macOS') {
             exec('kill -9 ' . $pid, $output, $returnVar);
-        } elseif ($isWindows) {
+        } elseif ($ystem === 'Windows') {
             exec('taskkill /F /PID ' . $pid, $output, $returnVar);
-        } elseif ($isLinux) {
+        } elseif ($ystem === 'Linux') {
             exec('kill -9 ' . $pid, $output, $returnVar);
         }
 
@@ -264,13 +417,11 @@ function runProject(): void
     }
 
     // Démarrer le serveur Symfony
-    io()->section('Démarrage du serveur Symfony...');
-
-    if ($isMac) {
+    if ($ystem === 'macOS') {
         exec('osascript -e \'tell application "Terminal" to do script "cd ' . getcwd() . ' && symfony server:start"\'', $output, $returnVar);
-    } elseif ($isWindows) {
+    } elseif ($ystem === 'Windows') {
         pclose(popen('start "" cmd.exe /K "cd /d ' . getcwd() . ' && symfony server:start"', 'r'));
-    } elseif ($isLinux) {
+    } elseif ($ystem === 'Linux') {
         exec('gnome-terminal -- bash -c "cd ' . getcwd() . ' && symfony server:start; exec bash"', $output, $returnVar);
         if ($returnVar !== 0) {
             exec('xterm -e "cd ' . getcwd() . ' && symfony server:start; exec bash"', $output, $returnVar);
@@ -278,24 +429,32 @@ function runProject(): void
     }
 
     io()->success('Serveur Symfony démarré.');
+}
 
-    sleep(2); // Attente de 2 secondes
+// laner les assets
+#[AsTask(description: 'Lancer le serveur de surveillance des assets')]
+function runAssets(): void
+{
+    io()->title('Lancement du serveur de surveillance des assets');
 
-    // Étape 2 : Démarrer la surveillance des assets
-    io()->section('Démarrage de la surveillance des assets...');
+    // Vérifier le système d'exploitation
+    $ystem = checkOS();
 
-    if ($isMac) {
+    // Démarrer le serveur de surveillance des assets
+    io()->section('Démarrage du serveur de surveillance des assets...');
+
+    if ($ystem === 'macOS') {
         exec('osascript -e \'tell application "Terminal" to do script "cd ' . getcwd() . ' && npm run watch"\'', $output, $returnVar);
-    } elseif ($isWindows) {
+    } elseif ($ystem === 'Windows') {
         pclose(popen('start "" cmd.exe /K "cd /d ' . getcwd() . ' && npm run watch"', 'r'));
-    } elseif ($isLinux) {
+    } elseif ($ystem === 'Linux') {
         exec('gnome-terminal -- bash -c "cd ' . getcwd() . ' && npm run watch; exec bash"', $output, $returnVar);
         if ($returnVar !== 0) {
             exec('xterm -e "cd ' . getcwd() . ' && npm run watch; exec bash"', $output, $returnVar);
         }
     }
 
-    io()->success('Surveillance des assets activée dans un nouveau terminal.');
+    io()->success('Serveur de surveillance des assets démarré.');
 }
 
 #[AsTask(description: 'Lancer le navigateur pour ouvrir le projet')]
